3a010d1a2cb5c55b670026494642fd69ae52568a
Gertjan Assies <gertjan.assies@gmail.com>
2025-01-27 15:11:40 +0100

    Added missing Schedule tests

--- src/test/scala/dev/gertjanassies/TestMedicineScheduleApi.scala
+++ src/test/scala/dev/gertjanassies/TestMedicineScheduleApi.scala
@@ -12,11 +12,22 @@ import zio.test._
 import zio.json.EncoderOps
 import zio.json.DecoderOps
 import dev.gertjanassies.medicate._
+import zio.http.Header.Origin
 
 object TestMedicineScheduleApi extends ZIOSpecDefault {
   val prefix = "test:api:schedule:"
   val medicine_prefix = "test:api:medicine:"
   def spec = {
+    val testMedicine1 = Medicine.create(
+      id = "1",
+      name = "Test",
+      dose = 1.0,
+      unit = "mg",
+      amount = Some(2.0),
+      stock = 10
+    )
+    val testMedicine2 = testMedicine1.copy(id = "2", name = "Test2")
+
     val testSchedule1 = MedicineSchedule(
       id = "1",
       medicineId = "1",
@@ -24,6 +35,7 @@ object TestMedicineScheduleApi extends ZIOSpecDefault {
       amount = 1.0
     )
     val testSchedule2 = testSchedule1.copy(id = "2", medicineId = "2")
+    val testSchedule3 = testSchedule1.copy(id = "3", medicineId = "1", time = "09:00")
 
     val testSuite = suite("Medicate Medicine Schedule API should ")(
       test("respond correctly to getting a list of medications") {
@@ -40,14 +52,20 @@ object TestMedicineScheduleApi extends ZIOSpecDefault {
       } @@ TestAspect.before(
         for {
           redis <- ZIO.service[Redis]
+          _ <- redis.set(s"${medicine_prefix}${testMedicine1.id}", testMedicine1.toJson)
+          _ <- redis.set(s"${medicine_prefix}${testMedicine2.id}", testMedicine2.toJson)
           _ <- redis.set(s"$prefix${testSchedule1.id}", testSchedule1.toJson)
           _ <- redis.set(s"$prefix${testSchedule2.id}", testSchedule2.toJson)
+          _ <- redis.set(s"$prefix${testSchedule3.id}", testSchedule3.toJson)
         } yield ()
       ) @@ TestAspect.after(
         for {
           redis <- ZIO.service[Redis]
           _ <- redis.del(s"$prefix${testSchedule1.id}")
           _ <- redis.del(s"$prefix${testSchedule2.id}")
+          _ <- redis.del(s"$prefix${testSchedule3.id}")
+          _ <- redis.del(s"${medicine_prefix}${testMedicine1.id}")
+          _ <- redis.del(s"${medicine_prefix}${testMedicine2.id}")
         } yield ()
       ),
       test("respond correctly to getting a single medication") {
@@ -122,6 +140,73 @@ object TestMedicineScheduleApi extends ZIOSpecDefault {
           _ <- redis.del(s"$prefix${testSchedule1.id}")
         } yield ()
       ),
+      test("be able to delete a schedule") {
+        for {
+          client <- ZIO.service[Client]
+          port <- ZIO.serviceWithZIO[Server](_.port)
+          _ <- TestServer.addRoutes(medicate.MedicineScheduleApi.routes)
+          response <- client.batched(
+            Request.delete(
+              URL.root.port(port) / "schedules" / testSchedule1.id
+            )
+          )
+        } yield assertTrue(response.status == Status.NoContent)
+      } @@ TestAspect.before(
+        for {
+          redis <- ZIO.service[Redis]
+          _ <- redis.set(s"$prefix${testSchedule1.id}", testSchedule1.toJson)
+        } yield ()
+      ) @@ TestAspect.after(
+        for {
+          redis <- ZIO.service[Redis]
+          _ <- redis.del(s"$prefix${testSchedule1.id}")
+        } yield ()
+      ),
+      test("be able to get a combined schedule") {
+        for {
+          client <- ZIO.service[Client]
+          port <- ZIO.serviceWithZIO[Server](_.port)
+          _ <- TestServer.addRoutes(medicate.MedicineScheduleApi.routes)
+          response <- client.batched(Request.get(URL.root.port(port) / "schedules" / "combined"))
+          body <- response.body.asString
+          combined = body.fromJson[List[medicate.CombinedSchedule]]
+        } yield assertTrue(response.status == Status.Ok) &&
+          assertTrue(combined.isRight)
+      } @@ TestAspect.before(
+        for {
+          redis <- ZIO.service[Redis]
+          _ <- redis.set(s"$prefix${testSchedule1.id}", testSchedule1.toJson)
+          _ <- redis.set(s"$prefix${testSchedule2.id}", testSchedule2.toJson)
+        } yield ()
+      ) @@ TestAspect.after(
+        for {
+          redis <- ZIO.service[Redis]
+          _ <- redis.del(s"$prefix${testSchedule1.id}")
+          _ <- redis.del(s"$prefix${testSchedule2.id}")
+        } yield ()
+      ),
+      test("CORS Config should allow for localhost") {
+        val cors_config = MedicineApi.config
+        assertTrue(
+          cors_config.allowedOrigin(Origin("http", "localhost", None)).isDefined
+        )
+        assertTrue(
+          cors_config
+            .allowedOrigin(Origin("http", "localhost", None))
+            .get
+            .headerName == "Access-Control-Allow-Origin"
+        )
+        assertTrue(
+          cors_config
+            .allowedOrigin(Origin("http", "localhost", None))
+            .get
+            .renderedValue == "http://localhost"
+        )
+
+        assertTrue(
+          cors_config.allowedOrigin(Origin("http", "example.com", None)).isEmpty
+        )
+      }
     ) @@ TestAspect.sequential
     if (scala.sys.env.contains("EMBEDDED_REDIS")) {
       testSuite.provideShared(
--- src/test/scala/dev/gertjanassies/TestMedicineScheduleRepository.scala
+++ src/test/scala/dev/gertjanassies/TestMedicineScheduleRepository.scala
@@ -13,7 +13,8 @@ object TestMedicineScheduleRepository extends ZIOSpecDefault {
   object ProtobufCodecSupplier extends CodecSupplier {
     def get[A: Schema]: BinaryCodec[A] = ProtobufCodec.protobufCodec
   }
-  val prefix = "test:repo:schedule:"
+  val schedule_prefix = "test:repo:schedule:"
+  val medicine_prefix = "test:repo:medicine:"
   def spec = {
     val testSuite = suite("MedicineRepository should")(
       test("be able to set and get a schedule") {
@@ -32,22 +33,99 @@ object TestMedicineScheduleRepository extends ZIOSpecDefault {
       } @@ TestAspect.after(
         for {
           redis <- ZIO.service[Redis]
-          _ <- redis.del(s"${prefix}test_set_get")
+          _ <- redis.del(s"${schedule_prefix}test_set_get")
         } yield ()
       ),
-    )
+      test("be able to delete a schedule") {
+        val s = MedicineSchedule.create(
+          id = "test_delete",
+          medicineId = "1",
+          time = "12:00",
+          amount = 1.0
+        )
+        for {
+          redis <- ZIO.service[Redis]
+          repo <- ZIO.service[MedicineScheduleRepository]
+          _ <- repo.create(s)
+          _ <- repo.delete(s.id)
+          gotten <- repo.getById(s.id)
+        } yield assert(gotten)(isNone)
+      } @@ TestAspect.after(
+        for {
+          redis <- ZIO.service[Redis]
+          _ <- redis.del(s"${schedule_prefix}test_delete")
+        } yield ()
+      ),
+      test("to return a combined schedule") {
+        val medicine1 = Medicine.create(
+          id = "1",
+          name = "test_medicine1",
+          dose = 1.0,
+          unit = "mg",
+          stock = 10,
+          amount = Some(1.0)
+        )
+        val medicine2 = medicine1.copy(id = "2", name = "test_medicine2")
+        val schedule1 = MedicineSchedule.create(
+          id = "1",
+          medicineId = "1",
+          time = "12:00",
+          amount = 1.0
+        )
+        val schedule2 = schedule1.copy(id = "2", medicineId = "2")
+        val schedule3 = schedule1.copy(id = "3", time = "09:00")
+        val expected = List(CombinedSchedule(
+            time = "09:00",
+            medicines = List(
+              (Some(medicine1), 1.0),
+            ),
+          ), CombinedSchedule(
+            time = "12:00",
+            medicines = List(
+              (Some(medicine1), 1.0),
+              (Some(medicine2), 1.0)
+            )
+          )
+        )
+        for {
+          redis <- ZIO.service[Redis]
+          m_repo <- ZIO.service[MedicineRepository]
+          _ <- m_repo.create(medicine1)
+          _ <- m_repo.create(medicine2)
+          ms_repo <- ZIO.service[MedicineScheduleRepository]
+          _ <- ms_repo.create(schedule1)
+          _ <- ms_repo.create(schedule2)
+          _ <- ms_repo.create(schedule3)
+          actual <- ms_repo.getSchedule()
+          _ <- ZIO.succeed(println(actual))
+          _ <- ZIO.succeed(println(expected))
+        } yield (assert(actual)(equalTo(expected))
+        )
+      } @@ TestAspect.after(
+        for {
+          redis <- ZIO.service[Redis]
+          _ <- redis.del(s"${schedule_prefix}1")
+          _ <- redis.del(s"${schedule_prefix}2")
+          _ <- redis.del(s"${schedule_prefix}3")
+          _ <- redis.del(s"${medicine_prefix}1")
+          _ <- redis.del(s"${medicine_prefix}2")
+        } yield ()
+      )
+    ) @@ TestAspect.sequential
     if (scala.sys.env.contains("EMBEDDED_REDIS")) {
       testSuite.provideShared(
         ZLayer.succeed[CodecSupplier](ProtobufCodecSupplier),
         EmbeddedRedis.layer,
         Redis.singleNode,
-        MedicineScheduleRepository.layer("test:repo:schedule:")
+        MedicineRepository.layer(medicine_prefix),
+        MedicineScheduleRepository.layer(schedule_prefix)
       )
     } else {
       testSuite.provideShared(
         ZLayer.succeed[CodecSupplier](ProtobufCodecSupplier),
         Redis.local,
-        MedicineScheduleRepository.layer("test:repo:schedule:")
+        MedicineRepository.layer(medicine_prefix),
+        MedicineScheduleRepository.layer(schedule_prefix)
       )
     }
   }
